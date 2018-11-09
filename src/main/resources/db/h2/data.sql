7c4e2d8886b10823dacd932cb6510a8ba18655ee
gengmr <gengmr@BJJTWB17B.centaline.com.cn>
2018-11-09 09:51:25 +0800

    修改SQL

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO `coder` (`id`, `name`, `sex`, `age`, `address`, `phone`) VALUES ('1', '刘金龙', '男', '18', '唐山', '');
+INSERT INTO `coder` (`id`, `name`, `sex`, `age`, `address`, `phone`) VALUES ('1', '黄小明', '男', '18', '北京', '');
+INSERT INTO `coder` (`id`, `name`, `sex`, `age`, `address`, `phone`) VALUES ('2', '杨莹', '女', '19', '上海', '');
